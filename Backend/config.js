b9bd1acdba7742e7c12dd16bd478ed5e88dff416
Pritish Bhakat <pritishbhakat@gmail.com>
2023-09-26 01:29:06 +0530

    change password

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoURL='mongodb+srv://pritishbhakat:root@cluster0.akqouoz.mongodb.net/?retryWrites=true&w=majority'
+export const mongoURL='mongodb+srv://pritishbhakat:<password>@cluster0.akqouoz.mongodb.net/?retryWrites=true&w=majority'
