45e8299e78192c049c5a7de6b37c56e544f2a065
Pritish Bhakat <pritishbhakat@gmail.com>
2023-10-30 23:47:55 +0530

    env updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,14 @@
+import dotenv from 'dotenv'
 import express, { response } from "express";
-import {PORT,mongoURL} from "./config.js"
+// import {PORT,mongoURL} from "./config.js"
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
 import cors from 'cors';
 
+dotenv.config()
 const app = express();
+const PORT = process.env.PORT || 3000
+const mongoURL = process.env.mongoURL
 
 //Middleware for parsing request body 
 app.use(express.json());
@@ -24,7 +28,7 @@ app.use(cors());
 
 app.get('/',(request,response)=>{
     console.log(request);
-    return response.status(234).send('Welcome To MERN stack Tutorial');
+    return response.status(234).send('Welcome To Book Store');
 });
 
 app.use('/books',booksRoute);
--- README.md
+++ README.md
@@ -3,3 +3,10 @@ npm run dev
 
 # Frontend
 npm run dev
+
+
+# Install Backend
+npm i
+npm i cors
+npm i mongoose
+
